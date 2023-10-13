172da7f9aba1ae4c744c9b1389b13d0a9d392dc3
Eric Batiste <ericbatiste4@gmail.com>
2023-10-13 07:19:31 -0600

    Complete book.js and pass all tests in book-test.js

--- src/book.js
+++ src/book.js
@@ -1,12 +1,46 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return `The ${title}`
+}
+
+function buildMainCharacter(name, age, pronouns) {
+  return {
+    name: name,
+    age: age,
+    pronouns: pronouns
+  }
+}
+
+function saveReview(review, reviews) {
+  for (var i = 0; i < reviews.length; i++){
+    if (reviews[i] === review) return;
+  }
+  return reviews.push(review)
+}
+
+function calculatePageCount(title) {
+  for (var i = 0; i < title.length; i++){
+  }
+  return i * 20;
+}
+
+function writeBook(title, mainCharacter, genre) {
+  return {
+    title: title,
+    mainCharacter: mainCharacter,
+    pageCount: calculatePageCount(title),
+    genre: genre 
+  }
+}
+
+function editBook(book) {
+  return book.pageCount *= .75;
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
