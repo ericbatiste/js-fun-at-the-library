233950aea5839feeed8aa39f19673980584b3bee
Eric Batiste <ericbatiste4@gmail.com>
2023-10-14 18:09:23 -0600

    Complete 'createLibrary' and 'addBook'.

--- src/library.js
+++ src/library.js
@@ -1,8 +1,33 @@
+function createLibrary(name){
+  return {
+    name: name,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: []
+    }
+  }
+}
 
+function addBook(library, book){
+  if (book.genre === 'fantasy') {
+    return library.shelves.fantasy.push(book);
+  } else if (book.genre === 'fiction') {
+    return library.shelves.fiction.push(book);
+  } else if (book.genre === 'nonFiction') {
+    return library.shelves.nonFiction.push(book);
+  } else {
+    return
+  };
+}
+
+function checkoutBook(library, title, genre){
+
+}
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook,
+  createLibrary,
+  addBook,
+  checkoutBook,
   // takeStock
 };
