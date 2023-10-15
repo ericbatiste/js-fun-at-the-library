b50388dc51c5a20f4698285d22194e635d5b322d
Eric Batiste <ericbatiste4@gmail.com>
2023-10-14 20:54:45 -0600

    Complete checkoutBook() and takeStock().

--- src/library.js
+++ src/library.js
@@ -22,12 +22,45 @@ function addBook(library, book){
 }
 
 function checkoutBook(library, title, genre){
+  var successMsg = `You have now checked out ${title} from the ${library.name}.`
+  var sorryMsg = `Sorry, there are currently no copies of ${title} available at the ${library.name}.`
 
+  if (title === "Dracula" && genre === "fantasy") {
+    library.shelves.fantasy.pop();
+    return successMsg;
+  } else if (title === "Born a Crime" && genre === "nonFiction") {
+    library.shelves.nonFiction.pop();
+    return successMsg;
+  } else if (title === "Pride and Prejudice" && genre === "fiction") {
+    library.shelves.fiction.pop();
+    return successMsg;  
+  } else {
+    return sorryMsg; 
+  }
+}
+
+function takeStock(library, genre) {
+  var fantasyShelf = library.shelves.fantasy;
+  var nonFictionShelf = library.shelves.nonFiction;
+  var fictionShelf = library.shelves.fiction;
+  var allBooks = fantasyShelf.length + nonFictionShelf.length + fictionShelf.length;
+
+  if (genre === 'fantasy') {
+    return `There are a total of ${fantasyShelf.length} fantasy books at the ${library.name}.`;
+  } else if (genre === 'nonFiction') {
+    return `There are a total of ${nonFictionShelf.length} nonFiction books at the ${library.name}.`;
+  } else if (genre === 'fiction') {
+    return `There are a total of ${fictionShelf.length} fiction books at the ${library.name}.`;
+  } else if (!genre) {
+    return `There are a total of ${allBooks} books at the ${library.name}.`;
+  } else {
+    return
+  }
 }
 
 module.exports = {
   createLibrary,
   addBook,
   checkoutBook,
-  // takeStock
+  takeStock
 };
