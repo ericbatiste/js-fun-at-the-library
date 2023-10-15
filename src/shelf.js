e70df9e3d4fa6aabff6dab410f1f98d96e536bb7
Eric Batiste <ericbatiste4@gmail.com>
2023-10-15 13:00:22 -0600

    Complete listTitles() and searchShelf()

--- src/shelf.js
+++ src/shelf.js
@@ -4,7 +4,9 @@ function shelfBook(book, shelf){
   if (shelf.length === 3) return;
   shelf.splice(0, 0, book)
 
-    // Code for if the books were in alphabetical order, which is what I thought was happening initially:
+    // Code for if the books were in alphabetical order, 
+    // which is what I thought was happening initially:
+    
     // title = book.title
     // shelf.push(book);
     // shelf.sort((a, b) => {
@@ -25,9 +27,26 @@ function unshelfBook(title, shelf) {
   }
 }
 
+function listTitles(shelf) {
+  var list = [];
+  for (var i = 0; i < shelf.length; i++) {
+    list.push(shelf[i].title);
+  }
+  return list.join(', ');
+}
+
+function searchShelf(shelf, title) {
+  for (var i = 0; i < shelf.length; i++) {
+    var titles = []; 
+    titles.push(shelf[i].title);
+  }
+  return titles.includes(title);
+}
+
+
 module.exports = {
   shelfBook,
   unshelfBook,
-  // listTitles,
-  // searchShelf
+  listTitles,
+  searchShelf
 };
